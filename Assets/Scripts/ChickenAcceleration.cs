5733b22902b26e4b4d1b4a90929bf82756103623
Remi Pierot <rfpierot91@gmail.com>
2017-11-14 15:54:25 +0100

    Update chicken script

--- Assets/Scripts/ChickenAcceleration.cs
+++ Assets/Scripts/ChickenAcceleration.cs
@@ -6,19 +6,30 @@ public class ChickenAcceleration : MonoBehaviour {
 	public Transform HeadNode;
 	public float Speed = 2.0f;
 
-	void Update ()
+	public Vector3 StartingPoint { get; private set; }
+
+	private void Start ()
 	{
+		StartingPoint = transform.position;
+	}
+
+	private void Update ()
+	{
+		// Get the forward of the camera on the XZ plane (horizontal, Right + Forward)
 		Vector3 currentHeadForward = HeadNode.forward;
 		currentHeadForward.y = 0.0f;
+		currentHeadForward.Normalize();
 
-		Vector3 currentDirection = HeadNode.localPosition;
+		// Get the current local position of the head inside the camera rig on the XZ plane (horizontal, Right + Forward)
+		Vector3 currentDirection = HeadNode.position - StartingPoint;
 		currentDirection.y = 0.0f;
 
-		Debug.LogError(Vector3.Angle(currentHeadForward, currentDirection));
-
-		if (currentDirection.magnitude > 0.15f && Vector3.Angle(currentHeadForward, currentDirection) < 45.0f)
+		// Activate movement
+		if (currentDirection.magnitude > 0.45f && Vector3.Angle(currentHeadForward, currentDirection) < 25.0f)
 		{
-			transform.position += currentHeadForward * Speed * Time.deltaTime;
+			Vector3 move = currentHeadForward * Speed * Time.deltaTime;
+			transform.position += move;
+			StartingPoint += move;
 		}
 	}
 }
