9784c957998cd329dcd6edbd68fc4c74ebdfcf67
Remi Pierot <rfpierot91@gmail.com>
2017-11-14 17:26:17 +0100

    Early navmesh + refactor Controller

--- Assets/Scripts/ChickenAcceleration.cs
+++ Assets/Scripts/ChickenAcceleration.cs
@@ -6,30 +6,36 @@ public class ChickenAcceleration : MonoBehaviour {
 	public Transform HeadNode;
 	public float Speed = 2.0f;
 
+	public SteamVR_TrackedController LeftCon;
+	public SteamVR_TrackedController RightCon;
+
 	public Vector3 StartingPoint { get; private set; }
 
 	private void Start ()
 	{
-		StartingPoint = transform.position;
+		StartingPoint = HeadNode.localPosition;
 	}
 
 	private void Update ()
 	{
+		if (LeftCon.triggerPressed || RightCon.triggerPressed)
+		{
+			StartingPoint = HeadNode.localPosition;
+		}
+
 		// Get the forward of the camera on the XZ plane (horizontal, Right + Forward)
 		Vector3 currentHeadForward = HeadNode.forward;
 		currentHeadForward.y = 0.0f;
-		currentHeadForward.Normalize();
 
 		// Get the current local position of the head inside the camera rig on the XZ plane (horizontal, Right + Forward)
-		Vector3 currentDirection = HeadNode.position - StartingPoint;
+		Vector3 currentDirection = HeadNode.localPosition - StartingPoint;
 		currentDirection.y = 0.0f;
 
 		// Activate movement
-		if (currentDirection.magnitude > 0.45f && Vector3.Angle(currentHeadForward, currentDirection) < 25.0f)
+		if (currentDirection.magnitude > 0.15f && Vector3.Dot(currentHeadForward, currentDirection) > 0.0f)
 		{
-			Vector3 move = currentHeadForward * Speed * Time.deltaTime;
+			Vector3 move = currentHeadForward.normalized * Speed * Time.deltaTime;
 			transform.position += move;
-			StartingPoint += move;
 		}
 	}
 }
