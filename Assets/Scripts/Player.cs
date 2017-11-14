5af939b73130c2ff28b74cdfc2981532063df461
Remi Pierot <rfpierot91@gmail.com>
2017-11-14 22:46:26 +0100

    Update using in scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
 	public int HealthPoint = 100;
 
-	private void Update()
-	{
-		Debug.LogError(HealthPoint);
-	}
 }
--- Assets/Scripts/ZombieAttack.cs
+++ Assets/Scripts/ZombieAttack.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ZombieAttack : MonoBehaviour {
+	private Player _Player;
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
 		{
-			Player p = other.gameObject.GetComponent<Player>();
-			p.HealthPoint--;
+			if(_Player == null)
+			{
+				_Player = other.gameObject.GetComponent<Player>();
+			}
+
+			_Player.HealthPoint--;
 		}
 	}
 }
